ceee54f6c3aac5fce4520e59c3b4dd8a3c2409f1
Bill Erickson <bill.erickson@gmail.com>
2023-07-20 07:42:28 -0500

    initial commit

---
+++ includes/class-integrate-flodesk-wpforms.php
@@ -0,0 +1,363 @@
+<?php
+/**
+ * Flodesk WPForms class.
+ *
+ * @package Integrate_Flodesk_WPForms
+ * @author CultivateWP
+ */
+
+/**
+ * Class Flodesk WPForms
+ *
+ * @package Integrate_Flodesk_WPForms
+ * @author CultivateWP
+ */
+class Integrate_Flodesk_WPForms extends WPForms_Provider {
+
+    /**
+     * API URL
+     * 
+     * @since 1.0.0
+     * 
+     * @var string
+     */
+    private $api_url = 'https://api.flodesk.com/v1/subscribers';
+
+    /**
+     * API Args
+     */
+    public function api_args( $api_key ) {
+        $auth = base64_encode( $api_key . ': ' );
+        $args = [
+            'headers' => [
+				'Content-Type' => 'application/json; charset=utf-8',
+                'Authorization' => 'Basic ' . $auth,
+                'User-Agent' => 'Integrate Flodesk & WPForms (https://cultivatewp.com)',
+            ]
+        ];
+        return $args;
+    }
+
+    /**
+	 * Holds the Flodesk account URL.
+	 *
+	 * @since   1.0.0
+	 *
+	 * @var     string
+	 */
+	private $api_key_url = 'https://app.flodesk.com/account/integration/api'; // @phpstan-ignore-line
+
+    /**
+     * Holds the Flodesk Register URL
+     * 
+     * @since 1.0.0
+     * 
+     * @var string
+     */
+    private $register_url = 'https://flodesk.com/c/63SZN2'; // @phpstan-ignore-line
+
+    /**
+     * Initialize
+     *
+     * @since 1.0.0
+     */
+    public function init() {
+
+		$this->version  = INTEGRATE_FLODESK_WPFORMS_VERSION;
+		$this->name     = 'Flodesk';
+		$this->slug     = 'flodesk';
+		$this->priority = 14;
+		$this->icon     = plugins_url( 'assets/flodesk-icon.png', INTEGRATE_FLODESK_WPFORMS_FILE );
+
+		if ( is_admin() ) {
+			add_filter( "wpforms_providers_provider_settings_formbuilder_display_content_default_screen_{$this->slug}", array( $this, 'builder_settings_default_content' ) );
+		}
+
+    }
+
+	/**
+	 * Processes and submits a WPForms Form entry to Flodesk,
+	 * based on the Form's settings at Marketing > Flodesk.
+	 *
+	 * @since   1.5.0
+	 *
+	 * @param   array $fields    List of fields with their data and settings.
+	 * @param   array $entry     Submitted entry values.
+	 * @param   array $form_data Form data and settings.
+	 * @param   int   $entry_id  Saved entry ID.
+	 */
+	public function process_entry( $fields, $entry, $form_data, $entry_id = 0 ) { 
+
+		// Only run if this form has a connection for this provider.
+		if ( empty( $form_data['providers'][ $this->slug ] ) ) {
+			return;
+		}
+
+
+		// Iterate through each Flodesk connection.  A WPForms Form can have one or more
+		// connection to a provider (i.e. Flodesk) configured at Marketing > Flodesk.
+		foreach ( $form_data['providers'][ $this->slug ] as $connection ) {
+
+			// Iterate through the WPForms Form field to Flodesk field mappings, to build
+			// the API query to subscribe to the Flodesk Form.
+			$body = array();
+			foreach ( $connection['fields'] as $flodesk_field => $wpforms_field ) {
+				// Skip if no Flodesk mapping specified for this WPForms Form field.
+				if ( empty( $wpforms_field ) ) {
+					continue;
+				}
+
+				// Fetch the field's value from the WPForms Form entry.
+				$value = $this->get_entry_field_value( $wpforms_field, $fields );
+
+				// Depending on the field name, store the value in the $args array.
+				switch ( $flodesk_field ) {
+					/**
+					 * Email
+					 * Name
+					 */
+					case 'email':
+					case 'name':
+						$body[ $flodesk_field ] = $value;
+						break;
+				}
+			}
+
+			// Skip if no email address field was mapped.
+			if ( ! array_key_exists( 'email', $body ) ) {
+				continue;
+			}
+
+			// Skip if the email address field is blank.
+			if ( empty( $body['email'] ) ) {
+				continue;
+			}
+
+			// Send data to Flodesk to subscribe the email address.
+			$providers = get_option( 'wpforms_providers', [] );
+			if ( ! empty( $providers[ $this->slug ][ $connection['account_id'] ]['api'] ) ) {
+				$args = $this->api_args( $providers[ $this->slug ][ $connection['account_id'] ]['api'] );
+				$args['body'] = json_encode( $body );
+				$response = wp_remote_post( $this->api_url, $args );
+			
+
+				// If the API response is an error, log it as an error.
+				if ( is_wp_error( $response ) ) {
+					wpforms_log(
+						'Flodesk',
+						sprintf(
+							'API Error: %s',
+							$response->get_error_message()
+						),
+						array(
+							'type'    => array( 'provider', 'error' ),
+							'parent'  => $entry_id,
+							'form_id' => $form_data['id'],
+						)
+					);
+
+					return;
+				}
+
+				// Log successful API response.
+				wpforms_log(
+					'Flodesk',
+					$response,
+					array(
+						'type'    => array( 'provider', 'log' ),
+						'parent'  => $entry_id,
+						'form_id' => $form_data['id'],
+					)
+				);
+			}
+		}        
+    }
+
+    /**
+	 * Returns the value for the given field in a WPForms form entry.
+	 *
+	 * @since   1.0.0
+	 *
+	 * @param   string $field      Field Name.
+	 * @param   array  $fields     Fields and their submitted form values.
+	 * @return  bool|string             Field Value
+	 */
+	private function get_entry_field_value( $field, $fields ) {
+
+		$field = explode( '.', $field );
+		$id    = $field[0];
+
+		// Determine the field ID's key that stores the submitted value for this field.
+		$key = 'value';
+		if ( ! empty( $field[1] ) ) {
+			$key = $field[1];
+		} elseif ( array_key_exists( 'value_raw', $fields[ $id ] ) ) {
+			// Some fields, such as checkboxes, radio buttons and select fields, may
+			// have a different value defined vs. the label. Using 'value_raw' will
+			// always fetch the value, if "Show Values" is enabled in WPForms,
+			// falling back to the label if "Show Values" is disabled.
+			$key = 'value_raw';
+		}
+
+		// Check if mapped form field has a value.
+		if ( empty( $fields[ $id ][ $key ] ) ) {
+			return false;
+		}
+
+		return $fields[ $id ][ $key ];
+
+	}    
+    /**
+	 * Output fields at WPForms > Settings > Integrations > Flodesk,
+	 * allowing the user to enter their Flodesk API Key.
+	 *
+	 * @since   1.5.0
+	 */
+	public function integrations_tab_new_form() {
+
+        
+		printf(
+			'<input type="text" name="apikey" placeholder="%s">',
+			sprintf(
+				/* translators: %s - current provider name. */
+				esc_html__( '%s API Key', 'integrate-flodesk-wpforms' ),
+				$this->name
+			)
+		);
+
+		printf(
+			'<input type="text" name="label" placeholder="%s">',
+			sprintf(
+				/* translators: %s - current provider name. */
+				esc_html__( '%s Account Nickname', 'wpforms-campaign-monitor' ),
+				$this->name
+			)
+		);        
+	}
+
+	/**
+	 * Default Content
+	 */
+	public function builder_settings_default_content( $content ) {
+		ob_start();
+		?>
+		<p>
+			<a href="<?php echo esc_url( $this->register_url ); ?>" class="wpforms-btn wpforms-btn-md wpforms-btn-orange" target="_blank" rel="noopener noreferrer">
+				<?php esc_html_e( 'Try Flodesk for Free', 'integrate-flodesk-wpforms' ); ?>
+			</a>
+		</p>
+		<?php
+
+		return $content . ob_get_clean();
+
+	}
+
+    /**
+	 * Authenticate with the provider API.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $data
+	 * @param string $form_id
+	 *
+	 * @return mixed id or error object
+	 */
+	public function api_auth( $data = [], $form_id = '' ) {
+        $request = wp_remote_get( $this->api_url, $this->api_args( $data['apikey'] ) );
+        if ( 200 !== wp_remote_retrieve_response_code( $request ) ) {
+            wpforms_log(
+				'Flodesk API error',
+				wp_remote_retrieve_response_message( $request ),
+				[
+					'type'    => [ 'provider', 'error' ],
+					'form_id' => isset( $form_id['id'] ) ? $form_id['id'] : 'Form ID not set',
+				]
+			);
+
+			return $this->error( 'API authorization error: ' . wp_remote_retrieve_response_message( $request ) );
+        } else {
+            $id                              = uniqid();
+            $providers                       = get_option( 'wpforms_providers', array() );
+            $providers[ $this->slug ][ $id ] = array(
+                'api'       => trim( $data['apikey'] ),
+                'label'     => sanitize_text_field( $data['label'] ),
+                //'client_id' => sanitize_text_field( $data['client_id'] ),
+                'date'      => time(),
+            );
+            update_option( 'wpforms_providers', $providers );
+    
+            return $id;            
+        }
+
+    }
+
+	/**
+	 * Retrieve provider account list fields.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $connection_id
+	 * @param string $account_id
+	 * @param string $list_id
+	 *
+	 * @return mixed array or error object
+	 */
+	public function api_fields( $connection_id = '', $account_id = '', $list_id = '' ) {
+
+        $provider_fields = array(
+			array(
+				'name'       => __( 'Flodesk: Email', 'integrate-flodesk-wpforms' ),
+				'field_type' => 'email',
+				'req'        => '1',
+				'tag'        => 'email',
+			),
+			array(
+				'name'       => __( 'Flodesk: First Name', 'integrate-flodesk-wpforms' ),
+				'field_type' => 'text',
+				'tag'        => 'first_name',
+			),
+			array(
+				'name'       => __( 'Flodesk: Last Name', 'integrate-flodesk-wpforms' ),
+				'field_type' => 'text',
+				'tag'        => 'last_name',
+			),
+		);
+        return $provider_fields;    
+	}
+
+    /**
+	 * Retrieve provider account lists.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $connection_id
+	 * @param string $account_id
+	 *
+	 * @return mixed array or WP_Error object.
+	 */
+	public function api_lists( $connection_id = '', $account_id = '' ) {
+        return [
+            [
+                'id' => $account_id,
+                'name' => 'Your Flodesk Account'
+            ]
+        ];
+    }
+
+    /**
+	 * Retrieve provider account list groups.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $connection_id
+	 * @param string $account_id
+	 * @param string $list_id
+	 *
+	 * @return mixed array or error object.
+	 */
+	public function api_groups( $connection_id = '', $account_id = '', $list_id = '' ) {
+        return [];
+    }
+
+}
+new Integrate_Flodesk_WPForms();
---
+++ integrate-flodesk-wpforms.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name: Integrate Flodesk and WPForms
+ * Plugin URI:  https://cultivatewp.com/plugins/integrate-flodesk-wpforms
+ * Description: Create Flodesk signup forms using WPForms
+ * Version:     1.0.0
+ * Author:      CultivateWP
+ * Author URI:  https://cultivatewp.com
+ * Text Domain: integrate-flodesk-wpforms
+ * Domain Path: /languages
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.  You may NOT assume that you can use any other
+ * version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * @package    Integrate_Flodesk_WPForms
+ * @since      1.0.0
+ * @copyright  Copyright (c) 2017, Bill Erickson
+ * @license    GPL-2.0+
+ */
+
+ // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'INTEGRATE_FLODESK_WPFORMS_FILE', plugin_basename( __FILE__ ) );
+define( 'INTEGRATE_FLODESK_WPFORMS_URL', plugin_dir_url( __FILE__ ) );
+define( 'INTEGRATE_FLODESK_WPFORMS_PATH', __DIR__ );
+define( 'INTEGRATE_FLODESK_WPFORMS_VERSION', '1.0.0' );
+
+/**
+ * Load the class
+ */
+function integrate_flodesk_wpforms() {
+    require_once plugin_dir_path( __FILE__ ) . '/includes/class-integrate-flodesk-wpforms.php';
+}
+add_action( 'wpforms_loaded', 'integrate_flodesk_wpforms' );
+
